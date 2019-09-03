69ba6eca618ae9d005122fd165b0558b7af92866
Gemhar33 <gemharedu@gmail.com>
2019-09-03 21:59:47 +0530

    added all the functionalities

---
+++ src/actions/techActions.js
@@ -0,0 +1,79 @@
+import {
+	GET_TECHS,
+	ADD_TECH,
+	DELETE_TECH,
+  TECHS_ERROR,
+  SET_LOADING,
+  
+} from './types';
+
+
+export const getTechs = () => async dispatch => {
+	try {
+		setLoading();
+		const res = await fetch('/techs');
+		const data = await res.json();
+
+		dispatch({
+			type: GET_TECHS,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: error.response.statusText,
+		});
+	}
+};
+
+
+export const addTech = tech => async dispatch => {
+	console.log(tech);
+	try {
+		setLoading();
+		const res = await fetch('/techs', {
+			method: 'POST',
+			body: JSON.stringify(tech),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		const data = await res.json();
+
+		dispatch({
+			type: ADD_TECH,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: error.response.statusText,
+		});
+	}
+};
+
+export const deleteTech = id => async dispatch => {
+	try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: 'DELETE'
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id
+    });
+	} catch (error) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: error.response.statusText,
+		});
+	}
+};
+
+export const setLoading = () => {
+	return {
+		type: SET_LOADING,
+	};
+};
---
+++ src/components/techs/TechSelectOptions.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { getTechs } from '../../actions/techActions';
+
+const TechSelectOptions = ({ getTechs, tech: { techs, loading } }) => {
+	useEffect(() => {
+		getTechs();
+		// eslint-disable-next-line
+	}, []);
+	return (
+		!loading &&
+		techs !== null &&
+		techs.map(tech => (
+			<option key={tech.id} value={`${tech.firstName} ${tech.lastName}`}>
+				{tech.firstName} {tech.lastName}
+			</option>
+		))
+	);
+};
+
+TechSelectOptions.propTypes = {
+	getTechs: PropTypes.func.isRequired,
+	tech: PropTypes.object,
+};
+
+const mapStateToProps = state => {
+	return {
+		tech: state.tech,
+	};
+};
+export default connect(
+	mapStateToProps,
+	{ getTechs }
+)(TechSelectOptions);
